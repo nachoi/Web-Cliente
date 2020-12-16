710e0583b54b6a4f963ce31c55c42e63fe42bf73
Daniel Rello <32819186+carthon@users.noreply.github.com>
2020-12-16 13:17:29 +0100

    Detailing with Bootstrap

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,39 @@
 export const products = [
     {
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
+      name: "Wonder Woman 1984",
+      price: 10,
+      image: "WonderWoman",
+      description: '18/12/2020 (ES) Fantasía, Acción, Aventura 2h 31m'
     },
     {
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
+      name: "Los Croods: Una nueva Era",
+      price: 10,
+      image: "Croods",
+      description: "23/12/2020 (ES) Aventura, Fantasía, Familia, Animación"
     },
     {
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
+      name: "Pinocho",
+      price: 10,
+      image: "Pinocho",
+      description: '18/9/2020 (ES) Fantasía, Familia, Aventura, Drama 2h 5m'
+    },
+    {
+      name: "Bob Esponja: Un héroe al rescate",
+      price: 10,
+      image: "BobEsponja",
+      description: '1/1/2021 (US) Animación, Fantasía, Aventura, Comedia, Familia'
+    },
+    {
+      name: "Monster Hunter",
+      price: 10,
+      image: "MonsterHunter",
+      description: '1/1/2021 (ES) Fantasía, Acción, Aventura 1h 44m'
+    },
+    {
+      name: "Este Cuerpo me sienta de Muerte",
+      price: 10,
+      image: "EsteCuerpo",
+      description: '30/12/2020 (ES) Terror, Suspense, Comedia, Fantasía'
     }
   ];
   
